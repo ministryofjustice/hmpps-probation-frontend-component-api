aa6235767f63391081a900d21d88cbba0b710edc
Lucas Cairns <lucas.cairns@gmail.com>
2025-04-15 13:30:23 +0100

    Fix linting issues

--- logger.ts
+++ logger.ts
@@ -4,6 +4,10 @@ import config from './server/config'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: !config.production })
 
-const logger = bunyan.createLogger({ name: 'HMPPS Probation Frontend Component Api', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({
+  name: 'HMPPS Probation Frontend Component Api',
+  stream: formatOut,
+  level: 'debug',
+})
 
 export default logger
