f5457473c45cf210d262a3a7a813ac8caef15bfb
Johan D. <johan.dindaine@justice.gov.uk>
2026-02-04 18:46:06 +0000

    removing the failing JS pointing towards a deleted element.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,3 @@ import * as mojFrontend from '@ministryofjustice/frontend'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
-
-document.addEventListener('DOMContentLoaded', function onLoad() {
-  const wrapper = document.getElementById('js-back-wrapper')
-  const link = document.createElement('a')
-  link.textContent = 'Back'
-  link.classList = 'govuk-back-link'
-  link.addEventListener('click', function handleClick(e) {
-    e.preventDefault()
-    window.history.back()
-  })
-
-  wrapper.appendChild(link)
-})
