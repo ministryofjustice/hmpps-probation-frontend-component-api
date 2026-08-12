8fbd5a34d06c311724e4c5acec1ba3759f1114d9
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-08-12 11:49:40 +0100

    Update getServicesForUser.ts

--- server/services/utils/getServicesForUser.ts
+++ server/services/utils/getServicesForUser.ts
@@ -9,14 +9,7 @@ export default (roles: string[]): Service[] => {
       heading: 'Accredited Programmes',
       href: config.serviceUrls.accreditedProgrammes.url,
       navEnabled: true,
-      enabled: () => 
-        userHasRoles(
-          [
-            Role.Probation,
-            Role.AccreditedProgrammes,
-          ],
-          roles,
-        ),
+      enabled: () => userHasRoles([Role.Probation, Role.AccreditedProgrammes], roles),
       accessibilityHeading: 'Accredited Programmes',
       accessibilityUrl: '/accessibility/accredited-programmes',
     },
